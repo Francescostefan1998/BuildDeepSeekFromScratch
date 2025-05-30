94002909ffa226b2bc55c65530acc11052a9b4e8
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-30 14:52:38 +0200

    download the dataset for the training

--- mixtureOfExperts.py
+++ mixtureOfExperts.py
@@ -3,3 +3,18 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 torch.manual_seed(42)
+# Download the tiny shakespeare dataset
+# !wget https://raw.githubusercontent.com/AviSoori1x/makeMoE/main/input.txt
+# import requests
+
+# url = "https://raw.githubusercontent.com/AviSoori1x/makeMoE/main/input.txt"
+# response = requests.get(url)
+
+# with open("input.txt", "w", encoding="utf-8") as f:
+#     f.write(response.text)
+# with open("input.txt", "r", encoding="utf-8") as f:
+#     text = f.read()
+# chars = sorted(list(set(text)))
+# vocab_size = len(chars)
+
+   
