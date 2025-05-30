df8ab72d22780fa23e4514908b676f077f45c21c
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-30 15:45:28 +0200

    selecting the expert

--- mixtureOfExperts.py
+++ mixtureOfExperts.py
@@ -21,11 +21,38 @@ torch.manual_seed(42)
 #Epert module
 class Expert(nn.Module):
     # An MLP is a simple liear layer followed by a non-linearity i.e. each Expert
-    def __init__(self, n_embd):
+    def __init__(self, n_embd, dropout):
         super().__init__()
-        self.net = nn.Sequential( # hold the actual layers, nn.Sequential stacks layers in order
-            nn.Linear(n_embd, 4* n_embd),
-            nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd),
-            nn.Dropout(dropout),
+        self.net = nn.Sequential( # hold the actual layers, nn.Sequential stacks layers in order, like a pipeline
+            nn.Linear(n_embd, 4* n_embd), # expands dimensionality by 4
+            nn.ReLU(), # adds non-linearity
+            nn.Linear(4 * n_embd, n_embd), # projects back to the original size
+            nn.Dropout(dropout), # regularization to avoid overfitting
         )
+
+    def forward(self, x):
+        return self.net(x)
+    
+# Understanding how gating works
+num_experts = 3
+top_k = 2 # this is the number of expert that the router will choose
+n_embed = 8 # Embedding dimension
+
+# Example multi-ead atttention output for a simple illustrative example, consider n_embed=32, context_length = 4
+mh_output = torch.randn(1, 4, n_embed) # pretending the ouput from a multihead attention, the 1 is the batch size, 4 is the number of tokens
+
+# maps from n_embed to num_expert
+# The following will actually be my router matrix
+topkgate_linear = nn.Linear(n_embed, num_experts) # nn.Linear(32, 4)
+
+# generating score , Expert selector matrix
+# the logits will likely be used later to pick the top-k experts for each token
+logits = topkgate_linear(mh_output) # this will perform the matrix multiplication to get e1, e2, e3
+print(logits)
+# implementing the top k load balancing
+top_k_logits, top_k_indices = logits.topk(top_k, dim=-1) # Get top-k experts
+print(top_k_logits, top_k_indices)
+# Now we replace the not selected values with negative infinity
+# And then we will apply softmax
+
+
