6ffa5f769d337409e5c39896e74ddfffb9e6b3d1
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-30 17:53:40 +0200

    matrix multiplication amongst experts

--- mixtureOfExperts.py
+++ mixtureOfExperts.py
@@ -118,3 +118,46 @@ top_k_gate = NoisyTopkRouter(n_embed, num_experts, top_k)
 gating_ouput, indices = top_k_gate(mh_output)
 print(gating_ouput.shape, gating_ouput, indices)
 
+# sparse mixture of experct module
+
+class SparseMoE(nn.Module):
+    def __init__(self, n_embed, num_experts, top_k):
+        super(SparseMoE, self).__init__()
+        self.router = NoisyTopkRouter(n_embed, num_experts, top_k)
+        self.experts = nn.ModuleList([Expert(n_embed) for _ in range(num_experts)])
+        self.top_k = top_k
+
+    def forward(self, x):
+        gating_output, indices = self.router(x)
+        final_output = torch.zeros_like(x)
+
+        # reshape inputs for batch processing
+        flat_x = x.view(-1, x.size(-1))
+        flat_gating_ouput = gating_output.view(-1, gating_output.size(-1))
+
+        # Process each expert in parallel
+        for i, expert in enumerate(self.experts):
+            # Create a mask for the inputs where the current expert is in the top-k
+            expert_mask = (indices == i).any(dim=-1)
+            flat_mask = expert_mask.view(-1)
+
+            if flat_mask.any():
+                expert_input = flat_x[flat_mask]
+                expert_output = expert(expert_input) # here happen the real matrix multiplication amongst experts
+
+                # Extract and apply gating scores
+                gating_scores = flat_gating_ouput[flat_mask, i].unsqueeze(1)
+                weighted_output = expert_output*gating_scores
+
+                # Update the final output additively by indexing and adding
+                final_output[expert_mask] += weighted_output.squeeze(1)
+
+        return final_output
+    
+num_experts = 3
+top_k = 2
+n_embd = 8
+mh_output = torch.randn(1, 4, n_embed) 
+top_k_gate = NoisyTopkRouter(n_embed, num_experts, top_k)
+gating_ouput, indices = top_k_gate(mh_output)
+print(gating_ouput.shape, gating_ouput, indices)
